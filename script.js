9d087c23d69a6a94fe245ca6e6b9e9f9635230ab
filipemedeiros12 <filipemedeiros144@gmail.com>
2021-06-05 12:11:25 -0300

    filipe4

--- script.js
+++ script.js
@@ -2,41 +2,52 @@ function calcular() {
 
     var nome = document.getElementById('nome').value;
 
-    var altura = parseFloat(document.getElementById('altura').value);
+    var altura = (document.getElementById('altura').value);
 
-    var peso = parseInt(document.getElementById('peso').value);
+    var peso = (document.getElementById('peso').value);
 
-    var mensagem = "seu imc é de "
+
 
     var classificacao = '';
 
-    valor = peso / (altura * altura);
+    if (nome !== '' && altura !== '' && peso !== '') {
+        var mensagem = "seu imc é de "
 
-    if (valor < 18.5) {
-        classificacao = 'abaixo do peso.';
-        document.getElementById('resultado').value = 
-        nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
-    } else if (valor < 25) {
-        classificacao = 'com peso ideal. Parabéns!!!';
-        document.getElementById('resultado').value = 
-        nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
-    } else if (valor < 30) {
-        classificacao = 'levemente acima do peso.';
-        document.getElementById('resultado').value = 
-        nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
-    } else if (valor < 35) {
-        classificacao = 'com obesidade grau I.';
-        document.getElementById('resultado').value = 
-        nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
-    } else if (valor < 40) {
-        classificacao = 'com obesidade grau II'
-        document.getElementById('resultado').value = 
-        nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
+        valor = peso / (altura * altura);
+
+        if (valor < 18.5) {
+            classificacao = 'abaixo do peso.';
+            document.getElementById('resultado').value =
+                nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
+        } else if (valor < 25) {
+            classificacao = 'com peso ideal. Parabéns!!!';
+            document.getElementById('resultado').value =
+                nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
+        } else if (valor < 30) {
+            classificacao = 'levemente acima do peso.';
+            document.getElementById('resultado').value =
+                nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
+        } else if (valor < 35) {
+            classificacao = 'com obesidade grau I.';
+            document.getElementById('resultado').value =
+                nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
+        } else if (valor < 40) {
+            classificacao = 'com obesidade grau II'
+            document.getElementById('resultado').value =
+                nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
+        } else {
+            (valor > 40.1)
+            classificacao = 'com obesidade grau III. Cuidado!!';
+            document.getElementById('resultado').value =
+                nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
+        }
+        
+
     } else {
-        (valor > 40.1)
-        classificacao = 'com obesidade grau III. Cuidado!!';
-        document.getElementById('resultado').value = 
-        nome + ' ' + mensagem + '' + valor.toFixed(2) + ' ' + classificacao;
+       document.getElementById('resultado').value = 'Preencha todos os campos!!!';
     }
+
+
+
 
 }
